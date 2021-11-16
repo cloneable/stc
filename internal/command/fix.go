9bada6adef1f9d88844107f6e1d3ffb83571c0fa
Folke Behrens <folke@gmail.com>
2021-11-16 10:03:51 +0100

    Allow any branchname with 'fix'

--- internal/command/fix.go
+++ internal/command/fix.go
@@ -4,8 +4,10 @@ import "github.com/spf13/cobra"
 
 func init() {
 	fixCmd := &cobra.Command{
-		Use:  "fix [<branch> <base>]",
-		Args: validBranchNames(stacker, 0, 2),
+		Use: "fix [<branch> <base>]",
+		// TODO: re-enable validation, add completion
+		Args: cobra.RangeArgs(0, 2), // validBranchNames(stacker, 0, 2),
+		//ValidArgsFunction: nil,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			return stacker.Fix(cmd.Context(), args...)
 		},
